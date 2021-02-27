eb4aaba46023ce85a07040de515c83a5d42b0d08
Steven <jchow446@gmail.com>
2021-02-26 22:37:34 -0800

    implementing buttons

--- schau_musicplayer.py
+++ schau_musicplayer.py
@@ -8,7 +8,7 @@ class Application (Frame):
         pygame.mixer.init()
         global paused
         #button functions/definitions
-        def play(song):
+        def play():
             paused = FALSE
             pygame.mixer.music.load("")
             pygame.mixer.music.play(loops=0)
@@ -16,17 +16,42 @@ class Application (Frame):
         def pause():
             paused = TRUE
             pygame.mixer.music.pause()
+        
+        def skip():
+            return
 
+        def prev():
+            return
+        
+        def stop():
+            pygame.mixer.music.stop()
+
+        #button images
+        self.play_btn_img = PhotoImage(file='imgs/play_img.png')
+        self.pause_btn_img = PhotoImage(file='imgs/pause_img.png')
+        self.skip_btn_img = PhotoImage(file='imgs/skip_img.png')
+        self.prev_btn_img = PhotoImage(file='imgs/prev_img.png')
+        self.stop_btn_img = PhotoImage(file='imgs/stop_img.png')
+        #initialize playlist box
+        self.playlist_box = Listbox(self, bg="white", fg="black", width=65, height=20)
+        
         #initialize buttons
-        self.play_button = Button(self, pady=20, padx=20 ,text ="Play")
-        self.pause_button = Button(self, pady=20, padx=20, text="Pause")
-    
+        self.buttons_frame = Frame(self)
+        self.play_btn = Button(image=self.play_btn_img, borderwidth=0, command=lambda:play())
+        self.pause_btn = Button(image=self.pause_btn_img, borderwidth=0)
+        self.skip_btn = Button(image=self.skip_btn_img, borderwidth=0)
+        self.prev_btn = Button(image=self.prev_btn_img, borderwidth=0)
+        self.stop_btn = Button(image=self.stop_btn_img, borderwidth=0, command=lambda:stop())
         #button grid placements
-        self.play_button.grid(row=1, column=2)    
-        self.pause_button.grid(row=1, column=1)  
-        self.pack()
+
+        self.play_btn.grid(row=1, column=2)
+        self.pause_btn.grid(row=1, column=3)
+        self.skip_btn.grid(row=1, column=4)
+        self.prev_btn.grid(row=1, column=1)
+        self.stop_btn.grid(row=1, column=5)
+        self.playlist_box.grid(row=2,column=3)
 root = tk.Tk()
 root.title("Music Player")
 app = Application(root)
-root.geometry("500x400")
+#root.geometry("500x400")
 root.mainloop()
