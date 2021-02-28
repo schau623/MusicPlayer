f22e39efac4b93f30d27b61499981e85976a590f
Steven <jchow446@gmail.com>
2021-02-28 15:06:39 -0800

    implemented song position slider

--- schau_musicplayer.py
+++ schau_musicplayer.py
@@ -5,6 +5,7 @@ import pygame
 from tinytag import TinyTag 
 import time
 from mutagen.mp3 import MP3
+import tkinter.ttk as ttk
 
 
 class Application (Frame):
@@ -26,6 +27,7 @@ class Application (Frame):
             pygame.mixer.music.play(loops=0)
             play_time()
 
+
         def pause(is_paused):
             global paused 
             paused = is_paused
@@ -37,6 +39,10 @@ class Application (Frame):
                 paused = True
         
         def skip():
+            self.position_bar.config(text='')
+            self.length_bar.config(text='')
+            self.slider_label.config(text='')
+            self.position_slider.config(value=0)
             next_song = self.playlist_box.curselection()
             next_song = next_song[0]+1
             pygame.mixer.music.load(playlist[next_song])
@@ -46,6 +52,10 @@ class Application (Frame):
             self.playlist_box.selection_set(next_song, last=None)
 
         def prev():
+            self.position_bar.config(text='')
+            self.length_bar.config(text='')
+            self.slider_label.config(text='')
+            self.position_slider.config(value=0)
             prev_song = self.playlist_box.curselection()
             prev_song = prev_song[0]-1
             pygame.mixer.music.load(playlist[prev_song])
@@ -55,6 +65,8 @@ class Application (Frame):
             self.playlist_box.selection_set(prev_song, last=None)
         
         def stop():
+            self.slider_label.config(text='')
+            self.position_slider.config(value=0)
             global paused
             paused = True
             pygame.mixer.music.stop()
@@ -82,7 +94,7 @@ class Application (Frame):
             self.playlist_box.delete(0, END)
             pygame.mixer.music.stop()
 
-        #position bar function
+        #song position time function
         def play_time():
             global paused
             if paused == False:
@@ -93,10 +105,39 @@ class Application (Frame):
                 self.position_bar.after(1000, play_time)
                 current_song = self.playlist_box.curselection()
                 song_mut = MP3(playlist[int(current_song[0])])
+
+                global song_length
                 song_length = song_mut.info.length
                 converted_song_length = time.strftime('%H:%M:%S',time.gmtime(song_length))
                 self.length_bar.config(text=converted_song_length)
                 self.length_bar.after(1000, play_time)
+                current_time+=1
+
+                if int(self.position_slider.get())==int(song_length):
+                    self.slider_label.config(text=converted_song_length)
+                
+                elif paused:
+                    pass
+                
+                elif int(self.position_slider.get()==int(current_time)):
+                    self.slider_position=int(song_length)
+                    self.position_slider.config(to=self.slider_position, value=int(current_time))
+                
+                else: #update slider to current position in song
+                    slider_position = int(song_length)
+                    self.position_slider.config(to=slider_position, value=int(self.position_slider.get()))
+                    converted_time = time.strftime('%H:%M:%S', time.gmtime(int(self.position_slider.get())))
+                    self.slider_label.config(text=converted_time)
+
+                    next_time = int(self.position_slider.get())+1
+                    self.position_slider.config(value=next_time)
+                    
+            self.slider_label.after(1000, play_time)
+
+        def position_slide(x):
+            song = self.playlist_box.curselection()
+            pygame.mixer.music.load(playlist[int(song[0])])
+            pygame.mixer.music.play(loops=0, start=int(self.position_slider.get()))
 
         #button images
         self.play_btn_img = PhotoImage(file='imgs/play_img.png')
@@ -129,25 +170,31 @@ class Application (Frame):
         self.stop_btn = Button(self.buttons_frame, image=self.stop_btn_img, borderwidth=0, command=lambda:stop())
 
         #Song position bar
-        self.position_bar = Label(self, width=10, text='', bd=1, relief=GROOVE)
-        self.length_bar = Label(self, width=10, text='', bd=1, relief=GROOVE)
-        #self.position_bar.pack(fill=X, side=BOTTOM, ipady=2)
-        
+        self.position_frame = Frame(self)
+        self.position_bar = Label(self.position_frame, width=10, text='', bd=1, relief=GROOVE)
+        self.length_bar = Label(self.position_frame, width=10, text='', bd=1, relief=GROOVE)
+        #Song position slider
+        self.position_slider = ttk.Scale(self.position_frame, from_=0, to_=100, orient=HORIZONTAL, value=0, command=position_slide, length=360)
+        self.slider_label = Label(self.position_frame, text='', width=10, bd=1, relief=GROOVE)
         #UI item placements
-        self.buttons_frame.grid(row=1, column = 3)
+        self.grid(row=0,column=0)
+        master.columnconfigure(0,weight=1)
+        master.rowconfigure(0,weight=1)
+        self.buttons_frame.grid(row=0, column = 3)
+        self.position_frame.grid(row=1, column=3, pady=10)
         self.play_btn.grid(row=1, column=2, padx=10)
         self.pause_btn.grid(row=1, column=3, padx=10)
         self.skip_btn.grid(row=1, column=4, padx=10)
         self.prev_btn.grid(row=1, column=1, padx=10)
         self.stop_btn.grid(row=1, column=5, padx=10)
         self.playlist_box.grid(row=3,column=3)
-        self.position_bar.grid(row=2,column=2)
-        self.length_bar.grid(row=2, column=4)
+        self.slider_label.grid(row=1,column=2)
+        self.length_bar.grid(row=1, column=4)
+        self.position_slider.grid(row=1, column=3)
         
-        self.pack()
-
+        #self.pack()
 root = tk.Tk()
 root.title("Music Player")
 app = Application(root)
-root.geometry("700x400")
+root.geometry("600x500")
 root.mainloop()
