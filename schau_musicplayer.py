08b06276fd1f78e624236e852226ba69765e09c7
Steven <jchow446@gmail.com>
2021-02-28 17:52:57 -0800

    Player slider crash fix

--- schau_musicplayer.py
+++ schau_musicplayer.py
@@ -16,7 +16,7 @@ class Application (Frame):
         global paused 
         paused = False
         playlist = []
-        
+        pygame.mixer.music.set_volume(0.1)
         #button/menu functions
         def play():
             global paused
@@ -39,7 +39,7 @@ class Application (Frame):
                 paused = True
         
         def skip():
-            self.position_bar.config(text='')
+            #self.position_bar.config(text='')
             self.length_bar.config(text='')
             self.slider_label.config(text='')
             self.position_slider.config(value=0)
@@ -52,7 +52,7 @@ class Application (Frame):
             self.playlist_box.selection_set(next_song, last=None)
 
         def prev():
-            self.position_bar.config(text='')
+            #self.position_bar.config(text='')
             self.length_bar.config(text='')
             self.slider_label.config(text='')
             self.position_slider.config(value=0)
@@ -66,13 +66,13 @@ class Application (Frame):
         
         def stop():
             self.slider_label.config(text='')
+            self.length_bar.config(text='')
             self.position_slider.config(value=0)
             global paused
             paused = True
             pygame.mixer.music.stop()
             self.playlist_box.selection_clear(ACTIVE)
-            self.position_bar.config(text='')
-            self.length_bar.config(text='')
+            #self.position_bar.config(text='')
 
         def add_song():
             #get song file directory
@@ -88,29 +88,35 @@ class Application (Frame):
 
         def remove_song():
             self.playlist_box.delete(ANCHOR)
+            playlist.remove(ANCHOR)
             pygame.mixer.music.stop()
 
         def clear_playlist():
             self.playlist_box.delete(0, END)
+            for i in range (len(playlist)):
+                playlist.pop()
             pygame.mixer.music.stop()
 
         #song position time function
         def play_time():
             global paused
             if paused == False:
+                #get current time/song position
                 current_time = pygame.mixer.music.get_pos()/1000
-                converted_time = time.strftime('%H:%M:%S', time.gmtime(current_time))
-                self.position_bar.config(text=converted_time)
+                converted_time = time.strftime('%M:%S', time.gmtime(current_time))
+                #self.position_bar.config(text=converted_time)
                 #update time
-                self.position_bar.after(1000, play_time)
+                #self.position_bar.after(1000, play_time)
+                
+
+                #get song length
                 current_song = self.playlist_box.curselection()
                 song_mut = MP3(playlist[int(current_song[0])])
-
                 global song_length
                 song_length = song_mut.info.length
-                converted_song_length = time.strftime('%H:%M:%S',time.gmtime(song_length))
+                converted_song_length = time.strftime('%M:%S',time.gmtime(song_length))
                 self.length_bar.config(text=converted_song_length)
-                self.length_bar.after(1000, play_time)
+                #self.length_bar.after(1000, play_time)
                 current_time+=1
 
                 if int(self.position_slider.get())==int(song_length):
@@ -171,15 +177,17 @@ class Application (Frame):
 
         #Song position bar
         self.position_frame = Frame(self)
-        self.position_bar = Label(self.position_frame, width=10, text='', bd=1, relief=GROOVE)
+        #self.position_bar = Label(self.position_frame, width=10, text='', bd=1, relief=GROOVE)
         self.length_bar = Label(self.position_frame, width=10, text='', bd=1, relief=GROOVE)
         #Song position slider
         self.position_slider = ttk.Scale(self.position_frame, from_=0, to_=100, orient=HORIZONTAL, value=0, command=position_slide, length=360)
         self.slider_label = Label(self.position_frame, text='', width=10, bd=1, relief=GROOVE)
+        
         #UI item placements
         self.grid(row=0,column=0)
         master.columnconfigure(0,weight=1)
         master.rowconfigure(0,weight=1)
+        #buttons
         self.buttons_frame.grid(row=0, column = 3)
         self.position_frame.grid(row=1, column=3, pady=10)
         self.play_btn.grid(row=1, column=2, padx=10)
@@ -187,7 +195,9 @@ class Application (Frame):
         self.skip_btn.grid(row=1, column=4, padx=10)
         self.prev_btn.grid(row=1, column=1, padx=10)
         self.stop_btn.grid(row=1, column=5, padx=10)
+        #playlist box
         self.playlist_box.grid(row=3,column=3)
+        #song pos slider
         self.slider_label.grid(row=1,column=2)
         self.length_bar.grid(row=1, column=4)
         self.position_slider.grid(row=1, column=3)
